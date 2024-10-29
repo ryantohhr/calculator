6710d46b739cd21bc2b23cb7bc15305af950aed0
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-29 14:44:50 +0800

    Implemented skeleton for construct and updateDisplay

--- script.js
+++ script.js
@@ -26,3 +26,54 @@ function operate(a, b, operator) {
             return divide(a, b);
     };
 }
+
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".display");
+let firstOperand = "";
+let secondOperand = "";
+let operator = null;
+let sign = "positive";
+construct();
+
+function construct() {
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.classList.contains("operand")) {
+                if (operator) {
+                    secondOperand += button.value;
+                }
+                else {
+                    firstOperand += button.value;
+                }
+            }
+            else if (button.classList.contains("clear")) {
+                firstOperand = "";
+                operator = "";
+                secondOperand = "";
+            }
+            else if (button.classList.contains("operator")) {
+                operator = button.value;
+            }
+            else if (button.classList.contains("equals")) {
+                firstOperand = Number(firstOperand);
+                secondOperand = Number(secondOperand);
+                firstOperand = operate(firstOperand, secondOperand, operator);
+                operator = "";
+                secondOperand = "";
+            }
+            else if (button.classList.contains("sign")) {
+                switchSign();
+            }
+            display.textContent = updateDisplay();
+        })
+    })
+}
+
+function updateDisplay() {
+    if (operator && secondOperand) {
+        return secondOperand;
+    }
+    else {
+        return firstOperand;
+    }
+}
