6ddf7ae3eccf54d68f0f235daf0066fd6f9f5a55
ryantohhr <ryantohhongrui@hotmail.com>
2024-10-29 21:06:24 +0800

    Added keyboard support

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
+    if (operator === "/" && b === 0) {
+        return "ERROR";
+    }
     switch (operator) {
         case "+":
             return add(a, b);
@@ -32,20 +35,23 @@ const display = document.querySelector(".display");
 let firstOperand = "";
 let secondOperand = "";
 let operator = null;
-let sign = "positive";
+let currentFirstSign = "positive";
+let currentSecondSign = "positive";
 let currentOperand = "";
+display.textContent = updateDisplay();
 construct();
+
+
+window.addEventListener('keydown', function(e) {
+    const key = document.querySelector(`button[value='${e.key}']`);
+    key.click();
+})
 
 function construct() {
     buttons.forEach((button) => {
         button.addEventListener('click', () => {
             if (button.classList.contains("operand")) {
-                if (operator) {
-                    secondOperand += button.value;
-                }
-                else {
-                    firstOperand += button.value;
-                }
+                appendOperand(button.value);
             }
             else if (button.classList.contains("clear")) {
                 firstOperand = "";
@@ -56,14 +62,11 @@ function construct() {
                 if (operator) {
                     firstOperand = Number(firstOperand);
                     secondOperand = Number(secondOperand);
-                    firstOperand = operate(firstOperand, secondOperand, operator);
+                    firstOperand = operate(firstOperand, secondOperand, operator).toString();
                     operator = "";
                     secondOperand = "";
                 }
                 operator = button.value;
-            }
-            else if (button.classList.contains("percent")) {
-                
             }
             else if (button.classList.contains("decimal")) {
                 if (currentOperand === "first" && !firstOperand.includes(".")) {
@@ -77,20 +80,30 @@ function construct() {
                 if (secondOperand) {
                     firstOperand = Number(firstOperand);
                     secondOperand = Number(secondOperand);
-                    firstOperand = operate(firstOperand, secondOperand, operator);
+                    firstOperand = operate(firstOperand, secondOperand, operator).toString();
                     operator = "";
                     secondOperand = "";
                 }
             }
-            else if (button.classList.contains("sign")) {
-                switchSign();
+            else if (button.classList.contains("delete")) {
+                if (operator) {
+                    secondOperand = secondOperand.substring(0, secondOperand.length - 1);
+                }
+                else {
+                    firstOperand = firstOperand.substring(0, firstOperand.length - 1);
+                }
             }
-
             if (operator) {
                 currentOperand = "second";
             }
             else {
                 currentOperand = "first";
+            }
+            if (firstOperand.length > 9) {
+                firstOperand = firstOperand.substring(0, 9);
+            }
+            if (secondOperand.length > 9) {
+                secondOperand = secondOperand.substring(0, 9);
             }
             display.textContent = updateDisplay();
         })
@@ -101,7 +114,29 @@ function updateDisplay() {
     if (operator && secondOperand) {
         return secondOperand;
     }
+    else if (!firstOperand) {
+        return "0";
+    }
     else {
         return firstOperand;
+    }
+}
+
+function appendOperand(value) {
+    if (operator) {
+        if (value === "0" && display.textContent === "0") {
+            firstOperand = "";
+        }
+        else {
+            secondOperand += value;
+        } 
+    }
+    else {
+        if (value === "0" && display.textContent === "0") {
+            firstOperand = "";
+        }
+        else {
+            firstOperand += value;
+        }
     }
 }
